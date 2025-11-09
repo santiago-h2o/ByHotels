877412239648e6ae84ef57f1815832e0855ec7df
santiago-h2o <maxcuello123@gmail.com>
2025-11-09 14:46:15 -0500

    Se crea el NavBar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import NavBar from './components/NavBar'
+import { useLocation } from 'react-router-dom'
 
 const App = () => {
+
+  const isOwnerPath = useLocation().pathname.includes('owner')
+
   return (
     <div>
-
+      {!isOwnerPath && <NavBar />}
     </div>
   )
 }
