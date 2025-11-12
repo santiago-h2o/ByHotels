6260bfaa8f66cbcb62bdc8706b1d24e79575f373
santiago-h2o <maxcuello123@gmail.com>
2025-11-11 19:22:44 -0500

    Se crea el footer

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,4 +1,5 @@
 import logo from './logo-light-transparent.png'
+import logoDark from './logo-dark-transparent.png'
 import searchIcon from './searchIcon.svg'
 import userIcon from './userIcon.svg'
 import calenderIcon from './calenderIcon.svg'
@@ -41,6 +42,7 @@ import totalRevenueIcon from "./totalRevenueIcon.svg";
 
 export const assets = {
     logo,
+    logoDark,
     searchIcon,
     userIcon,
     calenderIcon,
